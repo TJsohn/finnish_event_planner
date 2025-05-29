8d4c3d421306d2e2c94e3fc0b922d6792d157109
TJsohn <sohntaejeong@gmail.com>
2025-05-29 09:54:53 +0300

    add Home to index Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router';
 
+import Home from './pages/Home/Home';
 import EventList from './pages/EventList/EventList';
 import About from './pages/About/About';
 import AddEventForm from './pages/AddEventForm/AddEventForm';
@@ -9,12 +10,12 @@ import './App.css'
 
 const App = () => {
 
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Root />}>
-          <Route index element={<EventList />} />
+          <Route index element={<Home />} />
+          <Route path="/events" element={<EventList />} />
           <Route path="/add" element={<AddEventForm />} />
           <Route path="/about" element={<About />} />
         </Route>
