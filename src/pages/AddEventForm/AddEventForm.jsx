71a6b39d218654290d3c6bc1ae1df0e57459c70c
MaiTran41 <maitran.040196@gmail.com>
2025-05-29 10:04:45 +0300

    add AddEventForm

---
+++ src/pages/AddEventForm/AddEventForm.jsx
@@ -0,0 +1,24 @@
+const AddEventForm = () => {
+  return (
+    <>
+      <form>
+        <h1>Add Event Form</h1>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" id="" name="" />
+          <label htmlFor=""></label>
+          <input type="text" id="" name="" />
+          <label htmlFor=""></label>
+          <input type="text" id="" name="" />
+          <label htmlFor=""></label>
+          <input type="text" id="" name="" />
+
+          <button>Add</button>
+          <button>See more</button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default AddEventForm;
