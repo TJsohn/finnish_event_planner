161edc908fa6cec7f67f958d2f0758e0bc5b9669
MaiTran41 <maitran.040196@gmail.com>
2025-06-04 00:47:28 +0300

    add filter/ sort feat in EventList

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-// import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router";
-import useSpinningFavicon from './hooks/useSpinningFavicon';
-
+import useSpinningFavicon from "./hooks/useSpinningFavicon";
+import { useState, useEffect } from "react";
 import Home from "./pages/Home/Home";
 import EventList from "./pages/EventList/EventList";
 import About from "./pages/About/About";
@@ -9,16 +8,33 @@ import AddEventForm from "./pages/AddEventForm/AddEventForm";
 import Root from "./pages/Root";
 import "./App.css";
 import EventDetail from "./pages/EventDetail/EventDetail";
+import axios from "axios";
 
 const App = () => {
   useSpinningFavicon();
-  
+
+  const [eventsData, setEventsData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/events")
+      .then((res) => {
+        setEventsData(res.data);
+      })
+      .catch((error) => {
+        console.error("Fail to fectch", error);
+      });
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Root />}>
           <Route index element={<Home />} />
-          <Route path="/events" element={<EventList />} />
+          <Route
+            path="/events"
+            element={<EventList eventsData={eventsData} />}
+          />
           <Route path="/events/details" element={<EventDetail />} />
           <Route path="/add" element={<AddEventForm />} />
           <Route path="/about" element={<About />} />
---
+++ src/data/categories.js
@@ -0,0 +1,9 @@
+export const supportedCategory = {
+  culture: "Culture",
+  education: "Education",
+  sport: "Sport",
+  technology: "Technology",
+  entertainment: "Entertainment",
+  travel: "Travel",
+  all: "All", // pseudo category for display all events
+};
