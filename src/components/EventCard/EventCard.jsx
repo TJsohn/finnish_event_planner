14bc453e67b6847a54666a5f9de89a45b198c5be
antonriabokon <s2500139@edu.bc.fi>
2025-06-06 14:55:59 +0300

    event detail fixed

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,33 +1,42 @@
 import { Link } from "react-router";
 import styles from "./EventCard.module.css";
 import { emojisMap } from "../../data/emojisMap";
+import { supportedCategory } from "../../data/categories";
 
-const categoryLabels = {
-    culture: "Culture",
-    education: "Education",
-    sport: "Sport",
-    technology: "Technology",
-    entertainment: "Entertainment",
-    travel: "Travel",
-};
+const defaultImageUrl =
+  "https://cdn.pixabay.com/photo/2016/11/23/15/48/audience-1853662_1280.jpg";
 
-const defaultImageUrl = "https://cdn.pixabay.com/photo/2016/11/23/15/48/audience-1853662_1280.jpg";
+function formatDate(dateStr) {
+  if (!dateStr) return "";
+  const date = new Date(dateStr);
+  return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+}
 
 function formatEventDate(startDate, endDate) {
   if (!startDate) return "";
-  if (!endDate || startDate === endDate) return startDate;
-  return `${startDate} - ${endDate}`;
+  const formattedStart = formatDate(startDate);
+  const formattedEnd = formatDate(endDate);
+  if (!endDate || startDate === endDate) return formattedStart;;
+  return `${formattedStart} - ${formattedEnd}`;
 }
 
-const EventCard = ({ location, title, startDate, endDate, category, id, imageUrl }) => {
+const EventCard = ({
+  location,
+  title,
+  startDate,
+  endDate,
+  category,
+  id,
+  imageUrl,
+}) => {
   return (
     <div className={styles.eventCard}>
-        <div className={styles.categoryContainer}>
-            <p className={styles.categoryEmojis}>{emojisMap[category]}</p>
-            <span className={styles.categoryText}>
-                {categoryLabels[category] || category}
-            </span>
-        </div>
+      <div className={styles.categoryContainer}>
+        <p className={styles.categoryEmojis}>{emojisMap[category]}</p>
+        <span className={styles.categoryText}>
+          {supportedCategory[category] || category}
+        </span>
+      </div>
       <img
         src={imageUrl || defaultImageUrl}
         alt={title}
